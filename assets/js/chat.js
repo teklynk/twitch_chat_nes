8949b5355cac1205b7f07e0d8dc73cc9a13c443c
teklynk <ryanjones153@gmail.com>
2021-11-10 21:45:21 -0500

    simplified code base. using nes.min.js. added config page.

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,119 +1,153 @@
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    let results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-let channelName = getUrlParameter('channel');
-let romName = getUrlParameter('rom');
-
-function isInt(value) {
-    var x;
-    if (isNaN(value)) {
-        return false;
-    }
-    x = parseFloat(value);
-    return (x | 0) === x;
-}
-
 const client = new tmi.Client({
     channels: [channelName]
 });
 
 client.connect().catch(console.error);
 
+console.log(client.readyState());
+
 client.on('chat', (channel, user, message, self) => {
     if (user['message-type'] === 'chat') {
 
-        let timeLength = 100;
+        // ignore messages that do not start with !
+        if (self || !message.startsWith('!')) return;
 
-        messagePart2 = message.split(' ')[1];
+        let timeLength = 100; // default
+        let commandInteger;
 
-        if (isInt(messagePart2)) {
-            if (parseInt(messagePart2) < 5) {
-                timeLength = parseInt(messagePart2 + '000');
+        // split on spaces, grab the first item.
+        commandInteger = message.split(' ')[1];
+
+        // check if second part of message is an integer
+        if (isInt(commandInteger)) {
+            if (parseInt(commandInteger) <= 10) {
+                timeLength = parseInt(commandInteger + '00');
             }
         }
 
-        if (message.startsWith("!r")) {
-            pressPadButton(NesJs.Joypad.BUTTONS.RIGHT);
+        // D-Pad controls
 
-            setTimeout(function () {
-                releasePadButton(NesJs.Joypad.BUTTONS.RIGHT);
-            }, timeLength);
+        // check if message/command length is less than 5 characters
+        if (message.length <= 5) {
 
-            console.log('!r');
+            // right
+            if (message.startsWith("!r")) {
+                pressPadButton(NesJs.Joypad.BUTTONS.RIGHT);
+
+                setTimeout(function () {
+                    releasePadButton(NesJs.Joypad.BUTTONS.RIGHT);
+                }, timeLength);
+            }
+
+            // left
+            if (message.startsWith("!l")) {
+                pressPadButton(NesJs.Joypad.BUTTONS.LEFT);
+
+                setTimeout(function () {
+                    releasePadButton(NesJs.Joypad.BUTTONS.LEFT);
+                }, timeLength);
+            }
+
+            // up
+            if (message.startsWith("!u")) {
+                pressPadButton(NesJs.Joypad.BUTTONS.UP);
+
+                setTimeout(function () {
+                    releasePadButton(NesJs.Joypad.BUTTONS.UP);
+                }, timeLength);
+            }
+
+            // down
+            if (message.startsWith("!d")) {
+                pressPadButton(NesJs.Joypad.BUTTONS.DOWN);
+
+                setTimeout(function () {
+                    releasePadButton(NesJs.Joypad.BUTTONS.DOWN);
+                }, timeLength);
+            }
+
+            // a
+            if (message.startsWith("!a")) {
+                pressPadButton(NesJs.Joypad.BUTTONS.A);
+
+                setTimeout(function () {
+                    releasePadButton(NesJs.Joypad.BUTTONS.A);
+                }, timeLength);
+            }
+
+            // b
+            if (message.startsWith("!b")) {
+                pressPadButton(NesJs.Joypad.BUTTONS.B);
+
+                setTimeout(function () {
+                    releasePadButton(NesJs.Joypad.BUTTONS.B);
+                }, timeLength);
+            }
         }
 
-        if (message.startsWith("!l")) {
-            pressPadButton(NesJs.Joypad.BUTTONS.LEFT);
-
-            setTimeout(function () {
-                releasePadButton(NesJs.Joypad.BUTTONS.LEFT);
-            }, timeLength);
-
-            console.log('!l');
-        }
-
-        if (message.startsWith("!u")) {
-            pressPadButton(NesJs.Joypad.BUTTONS.UP);
-
-            setTimeout(function () {
-                releasePadButton(NesJs.Joypad.BUTTONS.UP);
-            }, timeLength);
-
-            console.log('!u');
-        }
-
-        if (message.startsWith("!d")) {
-            pressPadButton(NesJs.Joypad.BUTTONS.DOWN);
-
-            setTimeout(function () {
-                releasePadButton(NesJs.Joypad.BUTTONS.DOWN);
-            }, timeLength);
-
-            console.log('!d');
-        }
-
-        if (message.startsWith("!a")) {
-            pressPadButton(NesJs.Joypad.BUTTONS.A);
-
-            setTimeout(function () {
-                releasePadButton(NesJs.Joypad.BUTTONS.A);
-            }, 100);
-
-            console.log('!a');
-        }
-
-        if (message.startsWith("!b")) {
-            pressPadButton(NesJs.Joypad.BUTTONS.B);
-
-            setTimeout(function () {
-                releasePadButton(NesJs.Joypad.BUTTONS.B);
-            }, 100);
-
-            console.log('!b');
-        }
-
-        if (message.startsWith("!start")) {
+        // start
+        if (message === "!start") {
             pressPadButton(NesJs.Joypad.BUTTONS.START);
 
             setTimeout(function () {
                 releasePadButton(NesJs.Joypad.BUTTONS.START);
             }, 100);
-
-            console.log('!start');
         }
 
-        if (message.startsWith("!select")) {
+        // select
+        if (message === "!select") {
             pressPadButton(NesJs.Joypad.BUTTONS.SELECT);
 
             setTimeout(function () {
                 releasePadButton(NesJs.Joypad.BUTTONS.SELECT);
             }, 100);
+        }
 
-            console.log('!select');
+        // Mods only commands
+        if (modreset === 'true' && (user.mod || user.username === channelName)) {
+            resetGame();
+        } else if (modreset === 'false') {
+            resetGame();
+        }
+
+        if (modgame === 'true' && (user.mod || user.username === channelName)) {
+            changeGame();
+        } else if (modgame === 'false') {
+            changeGame();
+        }
+
+        function resetGame() {
+            // reset game
+            if (message === "!reset") {
+                setTimeout(function () {
+                    window.location.reload();
+                }, 100);
+            }
+        }
+
+        function changeGame() {
+            // change game
+            if (message.startsWith("!game")) {
+                // split on spaces, grab the first item.
+                let gameName = message.split(' ')[1];
+                let channelName = getUrlParameter('channel');
+                let size = getUrlParameter('size');
+                let modreset = getUrlParameter('mod-reset');
+                let modgame = getUrlParameter('mod-game');
+                let showmessages = getUrlParameter('show-messages');
+                let srcURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+                if (gameName) {
+                    setTimeout(function () {
+                        window.location.href = srcURL + "?channel=" + channelName + "&size=" + size + "&rom=" + gameName + "&mod-reset=" + modreset + "&mod-game=" + modgame + "&show-messages=" + showmessages;
+                    }, 100);
+                }
+            }
+        }
+
+        if (showMessages === 'true') {
+            // console output of user name and message
+            document.getElementById('chatCommand').innerText = user.username + ': ' + message;
+            console.log(user.username + ': ' + message);
         }
 
     }
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,47 @@
-var nes;
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    let results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+let channelName = getUrlParameter('channel');
+let romName = getUrlParameter('rom');
+let size = getUrlParameter('size');
+let modreset = getUrlParameter('mod-reset');
+let modgame = getUrlParameter('mod-game');
+let showmessages = getUrlParameter('show-messages');
+
+if (!channelName) {
+    alert('channel name not found in URL. ie: ?channel=MrCoolStreamer');
+}
+
+if (!romName) {
+    alert('rom name not found in URL. ie: &rom=invasion');
+}
+
+function isInt(value) {
+    let x;
+    if (isNaN(value)) {
+        return false;
+    }
+    x = parseFloat(value);
+    return (x | 0) === x;
+}
+
+let nes;
 
 function loadRom() {
-    var url;
+    let url;
 
-    if (!romName) {
-        alert('rom name not found in URL. ie: &rom=invasion');
-    } else {
+    try {
         url = './roms/' + romName + '.nes';
+    } catch (e) {
+        console.log('rom not found');
+        return;
     }
 
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.responseType = 'arraybuffer';
 
     request.onload = function () {
@@ -28,8 +60,9 @@ function loadRom() {
 }
 
 function run(buffer) {
+    let rom;
     try {
-        var rom = new NesJs.Rom(buffer);
+        rom = new NesJs.Rom(buffer);
     } catch (e) {
         console.log(e.toString());
         return;
@@ -46,7 +79,7 @@ function run(buffer) {
     try {
         nes.setAudio(new NesJs.Audio());
     } catch (e) {
-        console.log('Disables audio because this browser does not seems to support WebAudio.');
+        console.log('Disables audio because this browser does not seem to support WebAudio.');
     }
 
     window.onkeydown = function (e) {
